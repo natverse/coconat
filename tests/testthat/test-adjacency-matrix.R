be345682e278b65dd810a149248ad83b71551957
Gregory Jefferis <jefferis@gmail.com>
2023-02-02 13:47:08 +0000

    something funny about Matrix method dispatch

--- tests/testthat/test-adjacency-matrix.R
+++ tests/testthat/test-adjacency-matrix.R
@@ -5,12 +5,13 @@ test_that("partner_summary2adjacency_matrix works", {
 
   da2ds15=readRDS(system.file('sampledata/da2ds15.rds', package = 'coconat'))
 
-
-  expect_true(inherits(am <- partner_summary2adjacency_matrix(da2ds15, inputcol = 'partner', outputcol = 'bodyid'),'dgCMatrix'))
+  am <- partner_summary2adjacency_matrix(da2ds15, inputcol = 'partner', outputcol = 'bodyid')
+  expect_true(inherits(am,'dgCMatrix'))
   expect_snapshot(am2 <- partner_summary2adjacency_matrix(da2ds15, inputcol = 'partner', outputcol = 'bodyid', sparse = F))
-  library(Matrix)
+
   bl=c(`1796817841` = 380, `1797505019` = 207, `1796818119` = 489,
        `1827516355` = 197, `818983130` = 380)
-  expect_equal(colSums(am), bl)
-  expect_equal(colSums(am2), colSums(am))
+
+  expect_equal(Matrix::colSums(am), bl)
+  expect_equal(colSums(am2), Matrix::colSums(am))
 })
