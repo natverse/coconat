a9976d3207d2c50e029d51260a03b69bf316e68c
Gregory Jefferis <jefferis@gmail.com>
2023-02-10 18:22:31 +0000

    helper fun: prepare_cosine_matrix()

--- R/cosine.R
+++ R/cosine.R
@@ -32,3 +32,56 @@ cosine_sim <- function(x, sparse=FALSE, transpose=FALSE) {
   cosx=Matrix::cov2cor(cpx)
   if(sparse) cosx else as.matrix(cosx)
 }
+
+
+#' Cosine matrix utility functions
+#'
+#' @description These functions are intended for use by package authors rather
+#'   than end users.
+#'
+#' @param x A matrix or a named list of input/output matrices
+#' @param partners Whether to select input or output matrices when both are
+#'   available
+#' @param action Whether to zero out or drop any NA values in the cosine matrix
+#'   (these may be present when some columns have no entries)
+#'
+#' @return A matrix. When both inputs and outputs are used these will be
+#'   weighted by the total number of input and output synapses.
+#' @export
+prepare_cosine_matrix <- function(x, partners=c("inputs", "outputs"), action=c("zero", 'drop')) {
+  x <- fix_nas(x, action=action)
+  if(is.list(x)) {
+    x <- if(length(partners)==2) {
+      stopifnot(isTRUE(all.equal(dimnames(x$cin), dimnames(x$cout))))
+      (x$cin*x$win+x$cout*x$wout)/(x$win+x$wout)
+    }
+    else if(partners=='outputs')
+      x$cout
+    else
+      x$cin
+  }
+  x
+}
+
+fix_nas <- function(x, action=c("zero", 'drop')) {
+  action=match.arg(action)
+  if(is.list(x)) {
+    # calculate the fixed matrices
+    cico=sapply(x[c("cin", "cout")], fix_nas, action=action, simplify = F)
+    # replace the original ones (keeping the win/wout elements)
+    x[c("cin", "cout")]=cico
+    return(x)
+  }
+  isnax=is.na(x)
+  if(!any(isnax))
+    return(x)
+  if(action=='zero') {
+    x[isnax]=0
+    x
+  } else {
+    rs=rowSums(is.na(x))
+    cs=colSums(is.na(x))
+    # keep rows that have < ncol(x) NAs
+    x[rs!=ncol(x), cs!=nrow(x)]
+  }
+}
--- tests/testthat/test-cosine.R
+++ tests/testthat/test-cosine.R
@@ -2,4 +2,7 @@ test_that("cosine_sim works", {
   da2ds15=readRDS(system.file('sampledata/da2ds15.rds', package = 'coconat'))
   am=partner_summary2adjacency_matrix(da2ds15, inputcol = 'partner', outputcol = 'bodyid')
   expect_snapshot(cosine_sim(am))
+
+  expect_snapshot(pm <- prepare_cosine_matrix(cosine_sim(am, transpose = T)))
 })
+
