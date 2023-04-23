5809a289af3e903bf48e3c5c62f23a9c25283386
Gregory Jefferis <jefferis@gmail.com>
2023-04-23 21:58:32 +0100

    test polishing

--- tests/testthat/test-ids.R
+++ tests/testthat/test-ids.R
@@ -1,7 +1,8 @@
-test_that("id2har works", {
+test_that("id2char works", {
   expect_equal(id2char(1000), "1000")
   expect_equal(id2char(NA), NA_character_)
   expect_equal(id2char("1000"), "1000")
   expect_equal(id2char(1e5), "100000")
   expect_error(id2char(TRUE))
 })
+
